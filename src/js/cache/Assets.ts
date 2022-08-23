b44a511bbfa577be0f19116eb0c86f889575bf3e
bitWolfy <anon.wolfy@pm.me>
2022-08-22 23:08:36 -0700

    [Assets] Tweaks to the logo images

--- src/js/cache/Assets.ts
+++ src/js/cache/Assets.ts
@@ -10,9 +10,7 @@ export default class Assets {
                 this.ImagesInstance = {
                     logo32: null,
                     logo64: null,
-                    logoSVG: null,
                     kofi: null,
-                    spinner: null,
                     empty: null,
                 }
             }
@@ -24,9 +22,7 @@ class Images {
 
     logo32: string;
     logo64: string;
-    logoSVG: string;
     kofi: string;
-    spinner: string;
     empty: string;
 
 }
