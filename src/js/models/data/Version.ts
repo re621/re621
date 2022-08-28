3d816a81d56548fdd1a7c74e31f51d59f959c993
bitWolfy <anon.wolfy@pm.me>
2022-08-28 12:58:56 -0700

    [Version] Change the URL for version checking

--- src/js/models/data/Version.ts
+++ src/js/models/data/Version.ts
@@ -5,7 +5,7 @@ import XM from "../api/XM";
 
 export default class Version {
 
-    private static updateURL = "https://api.github.com/repos/re621/re621/releases/latest";
+    private static updateURL = "https://re621.bitwolfy.com/cache/version";
     private static semverRegex = /\d+\.\d+\.\d+/;
 
     public static Local = GM_info.script.version;
@@ -25,12 +25,10 @@ export default class Version {
                 method: "GET",
             });
 
-            let json: any;
-            try { json = JSON.parse(data.responseText); }
-            catch (error) { return passTime(); }
+            const version = data.responseText;
 
-            if (!json.tag_name) return passTime();
-            LocalStorage.Version.Remote = json.tag_name;
+            if (!version) return passTime();
+            LocalStorage.Version.Remote = version;
             LocalStorage.Version.Expires = Util.Time.now() + Util.Time.HOUR;
         } catch (error) {
             ErrorHandler.write("[RE621] Unable to check for updates", error);
@@ -43,14 +41,11 @@ export default class Version {
     }
 
     private static isRemoteAhead(local: string, remote: string): boolean {
-        console.log("comparing", local, remote);
-        console.log(!this.semverRegex.test(local) || !this.semverRegex.test(remote));
         if (!this.semverRegex.test(local) || !this.semverRegex.test(remote)) return false;
 
         const localParts = splitSemver(local),
             remoteParts = splitSemver(remote);
 
-        console.log(localParts, remoteParts);
         for (let i = 0; i < localParts.length; i++) {
             if (remoteParts[i] > localParts[i]) return true;
             else if (remoteParts[i] < localParts[i]) return false;
